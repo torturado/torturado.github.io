37743a67d9ebbe4155e33a0718f7938c7c9c1f57
torturado <73028283+torturado@users.noreply.github.com>
2023-05-25 17:57:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-// Obtener la zona horaria de España (UTC+2)
-var timezoneOffset = 2;
-
-// Fecha y hora objetivo (viernes 26 de mayo de 2023 a las 22:00 en España)
-var targetDate = new Date('May 26, 2023 22:00:00 GMT+0200').getTime();
+// Fecha y hora objetivo (viernes 26 de mayo de 2023 a las 22:00)
+var targetDate = new Date('May 26, 2023 22:00:00').getTime();
 
 // Actualizar el contador cada segundo
 var countdown = setInterval(function() {
   var now = new Date().getTime();
   var distance = targetDate - now;
-
-  // Ajustar la diferencia de zona horaria
-  distance -= timezoneOffset * 60 * 60 * 1000;
 
   // Cálculos para días, horas, minutos y segundos restantes
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
