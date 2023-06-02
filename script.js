1e25de1c5d78e2430f2dace445ff76493476e831
torturado <73028283+torturado@users.noreply.github.com>
2023-06-02 21:49:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 // Set the date we're counting down to
-var countDownDate = new Date("May 26, 2023 23:00:00").getTime();
+      var countDownDate = new Date("June 19, 2023 07:00:00 GMT+2").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
 
-  // Get today's date and time
-  var now = new Date().getTime();
+      // Update the countdown every 1 second
+      var x = setInterval(function() {
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+        // Get today's date and time
+        var now = new Date().getTime();
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Find the distance between now and the countdown date
+        var distance = countDownDate - now;
 
-  // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "Sacabau";
-  }
-}, 1000);
+        // Display the result in the element with id="countdown"
+        document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+        // If the countdown is finished, write some text
+        if (distance < 0) {
+          clearInterval(x);
+          document.getElementById("countdown").innerHTML = "EXPIRED";
+        }
+        }, 1000);
