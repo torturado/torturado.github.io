375a201a5a296d3c3db01b133406816aa975e901
torturado <73028283+torturado@users.noreply.github.com>
2023-10-31 09:36:34 +0000

    Update script.js

--- script.js
+++ script.js
@@ -29,13 +29,13 @@ document.addEventListener('DOMContentLoaded', function() {
         if (e.target.classList.contains('menu-item')) {
             switch (e.target.innerText) {
                 case 'Servicios':
-                    showModal('Servicios', 'Aquí encontrarás todos nuestros servicios...');
+                    showModal('Servicios', 'n HotMouse, nos enorgullece ofrecer una amplia variedad de ratones de ordenador calefactables para satisfacer todas tus necesidades. Desde modelos básicos hasta opciones más avanzadas, tenemos todo lo que necesitas para trabajar cómodamente durante horas. Además, todos nuestros productos vienen con una garantía de satisfacción del 100%, por lo que puedes comprar con confianza.');
                     break;
                 case 'Sobre nosotros':
-                    showModal('Sobre nosotros', 'Somos una empresa líder en el mercado...');
+                    showModal('Sobre nosotros', 'En HotMouse, estamos comprometidos a proporcionar ratones de ordenador calefactables de alta calidad que te permitan trabajar cómodamente durante horas. Nuestros productos están diseñados pensando en ti, y nos aseguramos de que cada uno de ellos cumpla con los más altos estándares de calidad. Además, nuestro equipo de atención al cliente está siempre disponible para ayudarte con cualquier pregunta o problema que puedas tener.');
                     break;
                 case 'Contáctenos':
-                    showModal('Contáctenos', 'Para contactarnos, puedes llamarnos o enviarnos un correo...');
+                    showModal('Contáctenos', 'Si tienes alguna pregunta o comentario sobre nuestros productos, no dudes en ponerte en contacto con nosotros. Puedes enviarnos un correo electrónico a info@hotmouse.com o llamarnos al +34 894. También puedes visitar nuestra tienda física en 123 Main Street, Anytown, USA.');
                     break;
             }
             dropdownMenu.style.display = 'none'; // Cerrar el menú
